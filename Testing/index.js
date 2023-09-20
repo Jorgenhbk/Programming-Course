00932621293b4dd2a36e6572ac5812731c8a0d71
Jørgen Eide Anskau <jorgeneideanskau@gmail.com>
2023-09-20 22:59:46 +0200

    moved the files inside a node folder

--- Testing/index.js
+++ Testing/index.js
@@ -1,8 +1,17 @@
 "use strict";
-let age = 18;
-let fullName = 'Tom';
-let isOfAge = true;
-let something;
-something = 123;
-console.log(something);
-console.log(something);
+let age = 12;
+let fullName = 'John Doe';
+let isOfAge = false;
+let something = 3;
+something = 'string';
+// Arrays
+let names = ['John', 'Jane', 'Jack'];
+//same but different
+let address = ["asda", "as"];
+let whatever = "🤨";
+// !!! Danger of "any"!!!
+let greeting = `3`;
+let somethingElse = greeting;
+console.log(typeof somethingElse);
+// Define a constant 'user' of type 'User', and initialize it with the string "John"
+const user = "John";
--- Testing/index.ts
+++ Testing/index.ts
@@ -1,9 +1,29 @@
-let age: number = 18;
-let fullName: string = 'Tom';
-let isOfAge: boolean = true;
-let something: unknown;
+let age: number = 12;
+let fullName: string = 'John Doe';
+let isOfAge: boolean = false;
+let something: any = 3;
 
-something = 123;
+something = 'string';
 
-console.log(something);
-console.log(something);
+// Arrays
+let names: string[] = ['John', 'Jane', 'Jack'];
+
+//same but different
+let address: string | string[] = ["asda", "as"];
+let whatever: string | string[] = "🤨";
+
+// !!! Danger of "any"!!!
+let greeting: any = `3`;
+
+let somethingElse: number = <number>greeting;
+
+console.log(typeof somethingElse);
+
+
+// Declare a custom type 'User' which can be either a string or an array of strings
+type User = string | string[];
+
+// Define a constant 'user' of type 'User', and initialize it with the string "John"
+const user: User = "John";
+
+
